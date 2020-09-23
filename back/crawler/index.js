698c7013907f48c1b45b7999151699c463ac5775
ken <ken0638@cchntek.com>
2020-09-23 18:19:45 +0800

    [fix]promise

--- back/crawler/index.js
+++ back/crawler/index.js
@@ -2,53 +2,50 @@ const request = require("request")
 const cheerio = require("cheerio")
 
 const startCrawler = (list = []) => {
-    const iteralList = () => {
-     const newItemList = []
 
-        for (let i = 0; i < list.length; i++) {
-         crawl(list[i])
-        }
-
-        console.log(newItemList);
-
+    const iteralList = async () => {
+        const lists = await Promise.all(
+            list.map(crawl)
+        )
+        console.log(lists);
+        return lists
     }
+
     iteralList()
+
     setInterval(iteralList, 60 * 1000)
 }
 
 
-const crawl =  function (itemNum ,callback) {
-    request({
-        url: `https://www.costco.com.tw/search?text=${itemNum}`, // 
-        method: "GET"
-    }, function (error, response, body) {
-        if (error || !body) {
-            return
-        }
-        const $ = cheerio.load(body) // 載入 body
-        const itemContainer = $(`
+function crawl(itemNum) {
+    const url = `https://www.costco.com.tw/search?text=${itemNum}`
+    return new Promise((resolve, reject) =>{
+        request({ url }, function (error, response, body) {
+            if (error || !body) {
+                return
+            }
+            const $ = cheerio.load(body) // 載入 body
+            const itemContainer = $(`
            #mainSearchComponentDivGridList
            .product-listing-container
            ul
            li
             `
-        )
-        const itemImg = itemContainer.find('a img').attr('src')
-        const itemName = itemContainer.find('.product-info-wrapper .product-list-details .product-name-container a').text()
-        const itemPrice = itemContainer.find('.product-info-wrapper .product-price-amount').text().trim()
-        const itemStockStatus = !itemContainer.find('.stock-status .out-of-stock-message').text()
-        const data = {
-            itemImg: `https://www.costco.com.tw/medias/sys_master${itemImg}`,
-            itemName,
-            itemPrice,
-            itemStockStatus
-        }
-
-            callback(data) 
-    })
+            )
+            const itemImg = itemContainer.find('a img').attr('src')
+            const itemName = itemContainer.find('.product-info-wrapper .product-list-details .product-name-container a').text()
+            const itemPrice = itemContainer.find('.product-info-wrapper .product-price-amount').text().trim()
+            const itemStockStatus = !itemContainer.find('.stock-status .out-of-stock-message').text()
+            const data = {
+                itemImg: `https://www.costco.com.tw/medias/sys_master${itemImg}`,
+                itemName,
+                itemPrice,
+                itemStockStatus
+            }
+            resolve({[itemNum]:data})
+        })
+    });
 }
-
-
 
 module.exports = startCrawler
 
