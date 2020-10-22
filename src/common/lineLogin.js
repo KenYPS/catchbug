d63566f0cb2e21db9cfddfb7faa7b30bae9472de
ken <ken0638@cchntek.com>
2020-10-22 14:19:19 +0800

    [fix]redirect url

--- src/common/lineLogin.js
+++ src/common/lineLogin.js
@@ -1,6 +1,6 @@
 
 const line_login = {
     client_id: 1654992288,
-    redirect_uri: process.env.NODE_ENV ?'https ://catchitems.herokuapp.com/': 'https://2a23c0f32881.ngrok.io/'
+    redirect_uri: process.env.NODE_ENV ?'https://catchitems.herokuapp.com/': 'https://2a23c0f32881.ngrok.io/'
 }
 module.exports = line_login
