89db8682425e744ff40a3a7f5155a83e291243cb
ken <ken0638@cchntek.com>
2020-10-23 18:13:05 +0800

    [fix]url

--- src/common/lineLoginConfig.js
+++ src/common/lineLoginConfig.js
@@ -1,8 +1,8 @@
 
 const lineLoginConfig = {
   client_id: 1654992288,
-  redirect_uri: 'https://2a23c0f32881.ngrok.io/'
-  // redirect_uri: 'https://catchitems.herokuapp.com/'
+  // redirect_uri: 'https://2a23c0f32881.ngrok.io/'
+  redirect_uri: 'https://catchitems.herokuapp.com/'
 }
 
 export default lineLoginConfig
